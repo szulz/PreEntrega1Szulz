04a9b4455bae425cce659a1476697f1f89018fdb
ezequiel <ezeszulz@hotmail.com>
2024-02-15 19:25:55 -0300

    modified:   src/components/NavBar/NavBar.jsx

--- src/components/NavBar/NavBar.jsx
+++ src/components/NavBar/NavBar.jsx
@@ -2,11 +2,11 @@ const NavBar = ({ label, listas, oListas }) => {
     return (
         <>
             <ul style={oListas}>
-                <li style={listas}><a href="">Category </a></li>
-                <li style={listas}><a href="">Category </a></li>
-                <li style={listas}><a href="">Category </a></li>
-                <li style={listas}><a href="">Category </a></li>
-                <li style={listas}><a href="">Category </a></li>
+                <li style={listas}><a href="">Clothes </a></li>
+                <li style={listas}><a href="">Technology </a></li>
+                <li style={listas}><a href="">Sports </a></li>
+                <li style={listas}><a href="">Music </a></li>
+                <li style={listas}><a href="">Food </a></li>
             </ul>
         </>
     )
